5a6c4fa62c8f103019d3f220ff17bc276145033e
Livio Brunner <livio.brunner@hotmail.com>
2016-06-12 19:23:49 +0200

    correct mysql queries for procedures

--- factories/SchülerFactory.php
+++ factories/SchülerFactory.php
@@ -1,20 +1,31 @@
-<?php
+<?php 
 class SchülerFactory extends RESTItem {
-	public function __construct(){
-		parent::__construct("schüler");
-	}
-	public function GetNotenBySchülerId($schülerId){
-		$result = mysqli_query($this->conn, 
-     		"CALL GetNotenBySchülerId(".$schülerId.")");
-		$noten = mysqli_fetch_all($result, MYSQLI_ASSOC);
-		return $noten;
-	}
-	public function GetAbsenzenFromThisYearBySchülerId($schülerId){
-		$result = mysqli_query($this->conn,
-			"CALL GetAbsenzenFromThisYearBySchülerId(".$schülerId.")");
-	    echo mysqli_error($this->conn);
-		$absenzen = mysqli_fetch_all($result, MYSQLI_ASSOC);
-		return $absenzen;
-	}
+    public function __construct() {
+        parent::__construct("schüler");
+    }
+    public function GetNotenBySchülerId($schülerId) {
+        $res = mysqli_multi_query($this->conn, "CALL GetNotenBySchülerId(".$schülerId.")");
+        echo mysqli_error($this->conn);
+        do {
+            if ($result = mysqli_store_result($this->conn)) {
+
+                $noten = mysqli_fetch_all($result, MYSQL_ASSOC);
+                $result->close();
+            }
+        } while (mysqli_next_result($this->conn));
+        return $noten;
+    }
+    public function GetAbsenzenFromThisYearBySchülerId($schülerId) {
+        $res = mysqli_multi_query($this->conn, "CALL GetNotenBySchülerId(".$schülerId.")");
+        echo mysqli_error($this->conn);
+        do {
+            if ($result = mysqli_store_result($this->conn)) {
+
+                $absenzen = mysqli_fetch_all($result, MYSQL_ASSOC);
+                $result->close();
+            }
+        } while (mysqli_next_result($this->conn));
+        return $absenzen;
+    }
 }
 ?>
